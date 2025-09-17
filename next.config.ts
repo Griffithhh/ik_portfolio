e54fcae6d786d2df9ecd992237b3274e7652c562
IVan <test@gmail.com>
2025-09-17 20:24:17 +0200

    upd

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/ik_portfolio",
-  assetPrefix: "./", // обязательно ведущий слеш
-  images: { unoptimized: true },
+  output: "export",               // статический экспорт
+  basePath: "/ik_portfolio",      // basePath для GitHub Pages
+  assetPrefix: "/ik_portfolio/",  // обязательный слэш в конце
+  images: { unoptimized: true },  // если используешь next/image
 };
 
 export default nextConfig;
+
